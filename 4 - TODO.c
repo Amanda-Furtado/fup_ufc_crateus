761c28ae489afd7f75eef9c3c941c478809205dd
Amanda Furtado <107702497+Amanda-Furtado@users.noreply.github.com>
2026-03-27 14:28:09 -0300

    Add files via upload

---
+++ 4 - TODO.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+
+// AULA 4 - Estruturas Condicionais
+/*Exercícios
+- Faça um programa para ler 3 notas de um
+aluno, calcular a média e dizer se ele está
+“Aprovado”, “Reprovado” ou “em Prova Final”.
+(Média menor que 4: “Reprovado”; média de 4 a
+7: “em Prova Final”; média a partir de 7:“Aprovado”).
+
+- Faça um programa para ler três números e
+escreva qual deles é o maior e qual é o menor.
+
+- Construa o programa completo do exemplo de
+Menu tanto com if/else quanto com switch/case.
+
+- Calcule o imposto e o salário líquido de um
+funcionário, baseado no salário bruto e no
+número de dependentes. O imposto será:
+    ● Salário bruto até R$ 2000,00 (isento)
+    ● Salário bruto de R$ 2000,01 a R$ 5000,00 (10% de imposto)
+    ● Cada dependente, reduz o imposto em R$100.
+    ● Se tiver mais de 3 dependentes, o imposto reduz em R$150 por dependente.
+    ● Acima de R$ 5000,00 (20% de imposto de renda). Neste caso, se não tiver dependentes,
+acrescenta-se R$200 no imposto. Crie um programa que avalie se um cliente pode
+receber um empréstimo baseado em renda mensal, valor do empréstimo solicitado, quantidade de parcelas,
+histórico de crédito (bom, regular ou ruim). Caso o valor da parcela do empréstimo ultrapassar
+em 30% a renda do cliente, então o emprestimo deverá ser negado. Caso
+o histórico seja bom, será aprovado. Se for regular, aprova somente se o valor da parcela for no máximo
+25% da renda e o valor do empréstimo seja no máximo 10x a renda. Agora, caso o histórico seja ruim, aprova
+somente se o valor da parcela for no máximo 20% da renda e tenha no máximo 12 parcelas. Aprova
+empréstimo acima de 50mil, somente para clientes com histórico bom. Mostre em qual regra o cliente se enquadrou.*/
+
+void set01()
+{
+    
+}
+
+void set02()
+{
+    
+}
+
+void set03()
+{
+    
+}
+
+void set04()
+{
+    
+}
+
+
+int main()
+{
+    
+    return 0;
+}
+
