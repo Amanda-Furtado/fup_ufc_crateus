3416aa71e254702eef3e04798fd06ee38e6d5722
Amanda Furtado <107702497+Amanda-Furtado@users.noreply.github.com>
2026-03-27 14:33:17 -0300

    Add files via upload

---
+++ LISTA 01 - Q1 TODO.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+// LISTA 01
+/*
+Escreva um programa que leia 3 números reais e os imprima em ordem crescente.
+*/
+
+
+int main()
+{
+    float n1, n2, n3, maior, menor;
+    scanf("%i %i %i", &n1, &n2, &n3);
+    maior = menor = n1;
+    
+    
+    
+        
+    return 0;
+}
+
