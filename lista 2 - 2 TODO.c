394986ca94a640ca3500f583b897b457bc5d521f
Amanda Furtado <107702497+Amanda-Furtado@users.noreply.github.com>
2026-03-30 14:49:14 -0300

    Add files via upload

---
+++ lista 2 - 2 TODO.c
@@ -0,0 +1,53 @@
+#include<stdio.h>
+
+
+void power()
+{
+    int m, n, og_m;
+
+    scanf("%i", &m);
+
+    scanf("%i", &n);
+
+    og_m = m;
+
+    for (int i = 1; i < n; i++)
+    {
+        m*=og_m;
+    }
+
+    printf("%i", m);
+}
+
+
+int main ()
+{
+    float m, n, og_m;
+
+    scanf("%f", &m);
+
+    scanf("%f", &n);
+
+    og_m = m;
+
+    if (n > 0)
+    {
+        for (int i = 1; i < n; i++)
+    {
+        m*=og_m;
+    }
+    }
+
+    else
+    {
+        n *= -1;
+        for (int i = 1; i < n; i++)
+    {
+        m = 1/m * 1/og_m;
+    }
+    }
+
+    printf("%.6f", m);
+
+    return 0;
+}
