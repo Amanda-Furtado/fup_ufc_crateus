aba9054b41c781d09aa647061a5f6a366fbdd8cc
Amanda Furtado <107702497+Amanda-Furtado@users.noreply.github.com>
2026-04-12 16:41:15 -0300

    Create 09.c

---
+++ LISTA 02/09.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+
+/*
+Afonso tem um grande entusiasmo pela Matematica. Certo dia, ele estava estudando sobre numeros
+naturais e percebeu que um numero da forma n^3 pode ser definido pela soma de n ımpares consecu-
+tivos. Por exemplo, 2^3 = 8 = 3 + 5. Dado um inteiro n, ele deseja saber quais seriam os n ımpares
+correspondentes tais que a soma deles seja igual a n3. Ele nao sabe programar e precisa de sua ajuda.
+*/
+
+
+int main(){
+    int n, y = 2, potencia, aux, valor;
+    
+    scanf("%i", &n);
+    aux = n;
+    
+    for(int i = 1; i < 3; i++){
+        aux = n * aux;
+    }
+    
+    potencia = aux;
+    
+    if(n%2 == 0){
+        valor = aux/n + 1;
+    }else{
+        valor = aux/n;
+    }
+    
+    aux = valor;
+    
+    for(int i = 1; i <= n; i++){
+        if(i % 2 != 0){
+            aux += valor - y;
+            printf("%i + ", valor - y);
+        }else{
+            aux += valor + y;
+            printf("%i + ", valor + y);
+            y += 2;
+        }
+        
+        if (aux == potencia){
+            printf("%i = %i\n", valor, potencia);
+            break;
+        }
+    }
+    
+    
+    return 0;
+}
